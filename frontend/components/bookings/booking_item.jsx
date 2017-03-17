e96449db57f87d61bfea0aa33e12a3ef94f05d62
Bo Katsarov <bozhidar.katsarov@gmail.com>
2017-03-17 16:15:01 -0700

    Added booking attributes to site

--- frontend/components/bookings/booking_item.jsx
+++ frontend/components/bookings/booking_item.jsx
@@ -9,7 +9,17 @@ class BookingItem extends React.Component {
 
     return (
       <li>
+        {this.props.booking.city}
+        {this.props.booking.title}
+        <br></br>
         {this.props.booking.num_travellers}
+        <br></br>
+        Dates: {this.props.booking.arrival_date} -
+        {this.props.booking.depart_date}
+        <br></br>
+        {this.props.booking.host_first_name}
+        <br></br>
+        {this.props.booking.host_last_name}
       </li>
     );
   }
